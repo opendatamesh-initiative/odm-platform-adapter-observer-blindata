34a2f32e03822761aa0ca62060a1f0696da7f880
Mattia Pennati <mattia.pennati@quantyca.it>
2023-05-30 10:04:32 +0200

    minor fixes

--- meta-service-server/src/main/java/org/opendatamesh/platform/up/metaservice/server/database/entities/Notification.java
+++ meta-service-server/src/main/java/org/opendatamesh/platform/up/metaservice/server/database/entities/Notification.java
@@ -1,34 +1,35 @@
 package org.opendatamesh.platform.up.metaservice.server.database.entities;
 
-
+import lombok.Data;
 import org.opendatamesh.platform.up.metaservice.resources.v1.NotificationStatus;
 
 import javax.persistence.*;
 import java.util.Date;
 
-@Entity
-@Table(name = "notification", schema = "PUBLIC")
+@Data
+@Entity(name = "Notification")
+@Table(name = "NOTIFICATION", schema = "PUBLIC")
 public class Notification {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "sequence_id")
+    @Column(name = "ID")
     private Long id;
 
     @Embedded
     private Event event;
 
-    @Column(name = "status")
+    @Column(name = "STATUS")
     @Enumerated(EnumType.STRING)
     private NotificationStatus status;
 
-    @Column(name = "processing_output")
+    @Column(name = "PROCESSING_OUTPUT")
     private String processingOutput;
 
-    @Column(name = "received_at")
+    @Column(name = "RECEIVED_AT")
     private Date receivedAt;
 
-    @Column(name = "processed_at")
+    @Column(name = "PROCESSED_AT")
     private Date processedAt;
 
     @PrePersist
@@ -36,51 +37,4 @@ public class Notification {
         receivedAt = new Date();
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Event getEvent() {
-        return event;
-    }
-
-    public void setEvent(Event event) {
-        this.event = event;
-    }
-
-    public NotificationStatus getStatus() {
-        return status;
-    }
-
-    public void setStatus(NotificationStatus status) {
-        this.status = status;
-    }
-
-    public String getProcessingOutput() {
-        return processingOutput;
-    }
-
-    public void setProcessingOutput(String processingOutput) {
-        this.processingOutput = processingOutput;
-    }
-
-    public Date getReceivedAt() {
-        return receivedAt;
-    }
-
-    public void setReceivedAt(Date receivedAt) {
-        this.receivedAt = receivedAt;
-    }
-
-    public Date getProcessedAt() {
-        return processedAt;
-    }
-
-    public void setProcessedAt(Date processedAt) {
-        this.processedAt = processedAt;
-    }
 }
