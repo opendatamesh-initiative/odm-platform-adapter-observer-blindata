228ec48e8dccbc8fa995c8f9fd3e7c219a999931
Mattia Pennati <mattia.pennati@quantyca.it>
2023-05-30 11:20:09 +0200

    minor fixes

--- meta-service-server/src/main/java/org/opendatamesh/platform/up/metaservice/server/database/entities/Event.java
+++ meta-service-server/src/main/java/org/opendatamesh/platform/up/metaservice/server/database/entities/Event.java
@@ -1,74 +1,31 @@
 package org.opendatamesh.platform.up.metaservice.server.database.entities;
 
 
+import lombok.Data;
+
 import javax.persistence.*;
 import java.util.Date;
 
 @Embeddable
+@Data
 public class Event {
 
-    @Column(name = "event_id")
+    @Column(name = "EVENT_ID")
     private Long id;
 
-    @Column(name = "event_type")
+    @Column(name = "EVENT_TYPE")
     private String type;
 
-    @Column(name = "event_entity_id")
+    @Column(name = "EVENT_ENTITY_ID")
     private String entityId;
 
-    @Column(name = "event_before_state")
+    @Column(name = "EVENT_BEFORE_STATE")
     private String beforeState;
 
-    @Column(name = "event_after_state")
+    @Column(name = "EVENT_AFTER_STATE")
     private String afterState;
 
-    @Column(name = "event_time")
+    @Column(name = "EVENT_TIME")
     private Date time;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public String getEntityId() {
-        return entityId;
-    }
-
-    public void setEntityId(String entityId) {
-        this.entityId = entityId;
-    }
-
-    public String getBeforeState() {
-        return beforeState;
-    }
-
-    public void setBeforeState(String beforeState) {
-        this.beforeState = beforeState;
-    }
-
-    public String getAfterState() {
-        return afterState;
-    }
-
-    public void setAfterState(String afterState) {
-        this.afterState = afterState;
-    }
-
-    public Date getTime() {
-        return time;
-    }
-
-    public void setTime(Date time) {
-        this.time = time;
-    }
 }
