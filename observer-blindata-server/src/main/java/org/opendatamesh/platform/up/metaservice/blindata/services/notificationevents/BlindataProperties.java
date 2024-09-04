9446ae4d4a8ca36b4dc75ff4c666245e38b58dca
gabriele <gabriele.fantini@blindata.io>
2024-09-04 12:27:20 +0200

    pr fix

---
+++ observer-blindata-server/src/main/java/org/opendatamesh/platform/up/metaservice/blindata/services/notificationevents/BlindataProperties.java
@@ -0,0 +1,125 @@
+package org.opendatamesh.platform.up.metaservice.blindata.services.notificationevents;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.List;
+
+@ConfigurationProperties(prefix = "blindata")
+public class BlindataProperties {
+
+    private String url;
+    private String user;
+    private String password;
+    private String tenantUUID;
+    private String roleUuid;
+    private String systemNameRegex;
+    private String systemTechnologyRegex;
+    private List<EventHandler> eventHandlers;
+
+    // Getters and Setters
+
+    public static class EventHandler {
+        private String eventType;
+        private String filter;
+        private List<String> activeUseCases;
+
+        // Getters and Setters
+
+        public String getEventType() {
+            return eventType;
+        }
+
+        public void setEventType(String eventType) {
+            this.eventType = eventType;
+        }
+
+        public String getFilter() {
+            return filter;
+        }
+
+        public void setFilter(String filter) {
+            this.filter = filter;
+        }
+
+        public List<String> getActiveUseCases() {
+            return activeUseCases;
+        }
+
+        public void setActiveUseCases(List<String> activeUseCases) {
+            this.activeUseCases = activeUseCases;
+        }
+
+        @Override
+        public String toString() {
+            return "EventHandler {" + "\n" +
+                    "\teventType='" + eventType + '\'' + ",\n" +
+                    "\tfilter='" + filter + '\'' + ",\n" +
+                    "\tactiveUseCases=" + activeUseCases + "\n" +
+                    '}';
+        }
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getTenantUUID() {
+        return tenantUUID;
+    }
+
+    public void setTenantUUID(String tenantUUID) {
+        this.tenantUUID = tenantUUID;
+    }
+
+    public String getRoleUuid() {
+        return roleUuid;
+    }
+
+    public void setRoleUuid(String roleUuid) {
+        this.roleUuid = roleUuid;
+    }
+
+    public String getSystemNameRegex() {
+        return systemNameRegex;
+    }
+
+    public void setSystemNameRegex(String systemNameRegex) {
+        this.systemNameRegex = systemNameRegex;
+    }
+
+    public String getSystemTechnologyRegex() {
+        return systemTechnologyRegex;
+    }
+
+    public void setSystemTechnologyRegex(String systemTechnologyRegex) {
+        this.systemTechnologyRegex = systemTechnologyRegex;
+    }
+
+    public List<EventHandler> getEventHandlers() {
+        return eventHandlers;
+    }
+
+    public void setEventHandlers(List<EventHandler> eventHandlers) {
+        this.eventHandlers = eventHandlers;
+    }
+}
--- observer-blindata-server/src/main/java/org/opendatamesh/platform/up/metaservice/blindata/services/notificationevents/NotificationEventManagerConfiguration.java
+++ observer-blindata-server/src/main/java/org/opendatamesh/platform/up/metaservice/blindata/services/notificationevents/NotificationEventManagerConfiguration.java
@@ -1,27 +1,25 @@
 package org.opendatamesh.platform.up.metaservice.blindata.services.notificationevents;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.opendatamesh.platform.up.metaservice.blindata.services.usecases.dataproduct_removal.DataProductRemovalFactory;
 import org.opendatamesh.platform.up.metaservice.blindata.services.usecases.dataproduct_upload.DataProductUploadFactory;
 import org.opendatamesh.platform.up.metaservice.blindata.services.usecases.dataproductversion_upload.DataProductVersionUploadFactory;
 import org.opendatamesh.platform.up.metaservice.blindata.services.usecases.policies_upload.PoliciesUploadFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Configuration
+@EnableConfigurationProperties(BlindataProperties.class)
+@Slf4j
 public class NotificationEventManagerConfiguration {
-
-    @Value("${blindata.eventHandlers}")
-    private String configs;
 
     @Autowired
     private DataProductUploadFactory dataProductUploadFactory;
@@ -32,29 +30,38 @@ public class NotificationEventManagerConfiguration {
     @Autowired
     private DataProductRemovalFactory dataProductRemovalFactory;
 
+    private final BlindataProperties blindataProperties;
+
+    public NotificationEventManagerConfiguration(BlindataProperties blindataProperties) {
+        this.blindataProperties = blindataProperties;
+    }
+
     @Bean
     public NotificationEventManager eventManager() throws JsonProcessingException {
         List<NotificationEventHandler> eventHandlers = new ArrayList<>();
 
-        EventHandlerConfig[] eventHandlerConfigs = new ObjectMapper().readValue(configs, EventHandlerConfig[].class);
-
-        for (EventHandlerConfig config : eventHandlerConfigs) {
+        for (BlindataProperties.EventHandler eventHandler : blindataProperties.getEventHandlers()) {
+            validateEventHandler(eventHandler);
             eventHandlers.add(new UseCasesExecutionTemplate(
-                    config.getActiveUseCases().contains("DATA_PRODUCT_UPLOAD") ? dataProductUploadFactory : null,
-                    config.getActiveUseCases().contains("DATA_PRODUCT_VERSION_UPLOAD") ? dataProductVersionUploadFactory : null,
-                    config.getActiveUseCases().contains("POLICIES_UPLOAD") ? policiesUploadFactory : null,
-                    config.getActiveUseCases().contains("DATA_PRODUCT_REMOVAL") ? dataProductRemovalFactory : null,
-                    config.eventType,
-                    config.filter
+                    eventHandler.getActiveUseCases().contains("DATA_PRODUCT_UPLOAD") ? dataProductUploadFactory : null,
+                    eventHandler.getActiveUseCases().contains("DATA_PRODUCT_VERSION_UPLOAD") ? dataProductVersionUploadFactory : null,
+                    eventHandler.getActiveUseCases().contains("POLICIES_UPLOAD") ? policiesUploadFactory : null,
+                    eventHandler.getActiveUseCases().contains("DATA_PRODUCT_REMOVAL") ? dataProductRemovalFactory : null,
+                    eventHandler.getEventType(),
+                    eventHandler.getFilter()
             ));
+            log.info("Activated event handler with the following configuration: \n{}", eventHandler.toString());
         }
         return new NotificationEventManager(eventHandlers);
     }
 
-    @Data
-    public static class EventHandlerConfig {
-        private String eventType;
-        private String filter;
-        private List<String> activeUseCases;
+    private void validateEventHandler(BlindataProperties.EventHandler eventHandler) {
+        if (!StringUtils.hasText(eventHandler.getEventType())) {
+            throw new RuntimeException("Event Handler Configuration is missing EventType");
+        }
+        if (CollectionUtils.isEmpty(eventHandler.getActiveUseCases())) {
+            throw new RuntimeException("Event Handler of " + eventHandler.getEventType() + " has an empty list of UseCases");
+        }
     }
+
 }
