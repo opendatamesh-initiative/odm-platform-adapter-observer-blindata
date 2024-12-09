27a7aa4bc03da6aea161eaf260f75d45ea5fc988
AGFaro <you@example.com>
2024-12-09 08:47:58 +0100

    Use recursive comparison for semantic linking test

--- observer-blindata-server/src/test/java/org/opendatamesh/platform/up/metaservice/blindata/schema_analyzers/semanticlinking/SemanticLinkManagerTest.java
+++ observer-blindata-server/src/test/java/org/opendatamesh/platform/up/metaservice/blindata/schema_analyzers/semanticlinking/SemanticLinkManagerTest.java
@@ -5,14 +5,8 @@ import com.google.common.io.Resources;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.opendatamesh.platform.up.metaservice.blindata.resources.blindataresources.BDPhysicalEntityRes;
-import org.opendatamesh.platform.up.metaservice.blindata.resources.blindataresources.BDPhysicalFieldRes;
-import org.opendatamesh.platform.up.metaservice.blindata.resources.blindataresources.BDSemanticLinkElement;
 
 import java.io.IOException;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 class SemanticLinkManagerTest {
 
@@ -38,21 +32,7 @@ class SemanticLinkManagerTest {
                 BDPhysicalEntityRes.class
         );
 
-        final Set<BDPhysicalFieldRes> expectedFields = expectedEntity.getPhysicalFields();
-        final Set<BDSemanticLinkElement> semanticLinkElementsFromExpectedFields = expectedFields.stream().findFirst().get().getLogicalFields().get(0).getSemanticLink().getSemanticLinkElements();
-        final Set<BDPhysicalFieldRes> enrichedFields = initialState.getPhysicalEntityRes().getPhysicalFields();
-        final Set<BDSemanticLinkElement> semanticLinkElementsFromEnrichedFields = initialState.getPhysicalEntityRes().getPhysicalFields().stream().findFirst().get().getLogicalFields().get(0).getSemanticLink().getSemanticLinkElements();
-
-        List<BDSemanticLinkElement> sortedSemanticLinkElementsFromExpectedFields = semanticLinkElementsFromExpectedFields.stream()
-                .sorted(Comparator.comparingInt(BDSemanticLinkElement::getOrdinalPosition))
-                .collect(Collectors.toList());
-
-        List<BDSemanticLinkElement> sortedSemanticLinkElementsFromEnrichedFields = semanticLinkElementsFromEnrichedFields.stream()
-                .sorted(Comparator.comparingInt(BDSemanticLinkElement::getOrdinalPosition))
-                .collect(Collectors.toList());
-
-        Assertions.assertThat(expectedFields.size()).isEqualTo(enrichedFields.size());
-        objectMapper.writeValueAsString(sortedSemanticLinkElementsFromEnrichedFields).equals(objectMapper.writeValueAsString(sortedSemanticLinkElementsFromExpectedFields));
+        Assertions.assertThat(expectedEntity).usingRecursiveComparison().ignoringCollectionOrder().isEqualTo(initialState.getPhysicalEntityRes());
 
     }
 }
