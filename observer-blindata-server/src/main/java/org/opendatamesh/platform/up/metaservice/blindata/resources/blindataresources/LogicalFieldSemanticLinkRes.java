e5212b6800e560bd92cfc67e67f6f9476a20dbf5
AGFaro <you@example.com>
2024-12-18 15:55:56 +0100

    Logical field semantic link res fix

--- observer-blindata-server/src/main/java/org/opendatamesh/platform/up/metaservice/blindata/resources/blindataresources/LogicalFieldSemanticLinkRes.java
+++ observer-blindata-server/src/main/java/org/opendatamesh/platform/up/metaservice/blindata/resources/blindataresources/LogicalFieldSemanticLinkRes.java
@@ -4,6 +4,9 @@ import io.swagger.v3.oas.annotations.media.Schema;
 
 public class LogicalFieldSemanticLinkRes {
 
+    @Schema(description = "The LogicalField resource identifier")
+    private String uuid;
+
     @Schema(description = "The semantic link associated to the logical field")
     private SemanticLinkHeaderRes semanticLink;
 
@@ -13,5 +16,13 @@ public class LogicalFieldSemanticLinkRes {
 
     public void setSemanticLink(SemanticLinkHeaderRes semanticLink) {
         this.semanticLink = semanticLink;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
     }
 }
