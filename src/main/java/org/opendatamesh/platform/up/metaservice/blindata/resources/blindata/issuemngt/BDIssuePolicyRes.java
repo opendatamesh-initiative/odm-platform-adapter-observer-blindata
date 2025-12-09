78538e8753e0cf2e2cff6131cee2f67dbcdcca7c
Alfio Giuliano Faro <100780364+AGFaro@users.noreply.github.com>
2025-12-09 16:15:24 +0100

    Added description support for issue policies (#136)

--- src/main/java/org/opendatamesh/platform/up/metaservice/blindata/resources/blindata/issuemngt/BDIssuePolicyRes.java
+++ src/main/java/org/opendatamesh/platform/up/metaservice/blindata/resources/blindata/issuemngt/BDIssuePolicyRes.java
@@ -6,6 +6,7 @@ import java.util.Date;
 public class BDIssuePolicyRes {
     private String uuid;
     private String name;
+    private String description;
     private String resourceIdentifier;
     private BDResourceTypeRes resourceType;
     private String resourceName;
@@ -50,6 +51,14 @@ public class BDIssuePolicyRes {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     public String getResourceIdentifier() {
@@ -113,6 +122,7 @@ public class BDIssuePolicyRes {
         return "BDIssuePolicyRes{" +
                 "uuid='" + uuid + '\'' +
                 ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
                 ", resourceIdentifier='" + resourceIdentifier + '\'' +
                 ", resourceType=" + resourceType +
                 ", resourceName='" + resourceName + '\'' +
