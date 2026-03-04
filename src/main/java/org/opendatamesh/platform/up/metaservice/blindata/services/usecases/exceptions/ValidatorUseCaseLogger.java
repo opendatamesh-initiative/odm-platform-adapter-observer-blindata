1bcd3e2bf5d804874edeb306ce549a52f0ca3954
gabriele <gabrielefantini97@gmail.com>
2026-03-04 15:03:13 +0100

    feat: enhance warning logging in ValidatorUseCaseLogger to include exception details and stack trace

--- src/main/java/org/opendatamesh/platform/up/metaservice/blindata/services/usecases/exceptions/ValidatorUseCaseLogger.java
+++ src/main/java/org/opendatamesh/platform/up/metaservice/blindata/services/usecases/exceptions/ValidatorUseCaseLogger.java
@@ -1,7 +1,11 @@
 package org.opendatamesh.platform.up.metaservice.blindata.services.usecases.exceptions;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.List;
+
+import org.springframework.util.StringUtils;
 
 public class ValidatorUseCaseLogger implements UseCaseLogger {
 
@@ -24,9 +28,20 @@ public class ValidatorUseCaseLogger implements UseCaseLogger {
 
     @Override
     public void warn(String message, Exception e) {
-        warnings.add(message);
+        String toAdd = message;
+        if (!StringUtils.hasText(toAdd) && e != null) {
+            toAdd = e.getClass().getSimpleName()
+                    + (e.getMessage() != null && !e.getMessage().isBlank() ? ": " + e.getMessage() : ": No message")
+                    + "\n" + getStackTraceString(e);
+        }
+        warnings.add(toAdd);
     }
 
+    private static String getStackTraceString(Throwable t) {
+        StringWriter sw = new StringWriter();
+        t.printStackTrace(new PrintWriter(sw));
+        return sw.toString();
+    }
 
     public List<String> getWarnings() {
         return warnings;
